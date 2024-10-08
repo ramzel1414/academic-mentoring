c82e35d172565fa0ced22fcebbff3c02edc9c140
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-08 21:50:25 +0800

    gamit tag git init para ma differentiate ang changes

---
+++ index.js
@@ -0,0 +1,4 @@
+// console.log('hello world');
+import getPosts from "./postController.js";
+
+console.log(getPosts());
---
+++ postController.js
@@ -0,0 +1,11 @@
+const posts = [
+  {id: 1, name: 'Post one'},
+  {id: 2, name: 'Post two'},
+  {id: 3, name: 'Post three'},
+]
+
+const getPosts = () => {
+  return posts
+}
+
+export default getPosts;
