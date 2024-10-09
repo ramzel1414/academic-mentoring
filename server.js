2447af4570235528a87471ea662e3bbf574e0ba6
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-09 13:46:04 +0800

    Simulate server errror

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import http from 'http';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {  
-  res.end('Hello from server')
+  res.writeHead(500, {'Content-type': 'application/json'});
+  res.end(JSON.stringify({message: 'server error'}))
 })
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
