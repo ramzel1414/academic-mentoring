525d02251efd81fce6e787c8afd8681dec86c3e9
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-09 16:28:20 +0800

    node: Simple routing (res.writeHead)

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {  
   // res.end('<h1>hello world</h1>');
   if(req.url == '/') {
+    res.writeHead(200, {'Content-type': 'text/html'})
     res.end('<h1>Homepage</h1>');
   } else if(req.url == '/about') {
+    res.writeHead(200, {'Content-type': 'text/html'})
     res.end('<h1>About page</h1>');
   } else {
+    res.writeHead(404, {'Content-type': 'text/html'})
     res.end('<h1>Not found</h1>');
   }
 })
