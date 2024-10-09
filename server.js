034dee485b2a3caae77b59f9720ac144367ed813
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-09 16:08:16 +0800

    node: Simple routing

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ import http from 'http';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {  
-  res.end('<h1>hello world</h1>');
-  // res.writeHead(500, {'Content-type': 'application/json'});
-  // res.end(JSON.stringify({message: 'server error'}))
+  // res.end('<h1>hello world</h1>');
+  if(req.url == '/') {
+    res.end('<h1>Homepage</h1>');
+  } else if(req.url == '/about') {
+    res.end('<h1>About page</h1>');
+  } else {
+    res.end('<h1>Not found</h1>');
+  }
 })
 
 server.listen(PORT, () => {
