fdd13c392a655800f2d2627221baa0d3416b4654
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-09 15:37:08 +0800

    node: install and use nodemon add .gitignore file for node_modules

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ import http from 'http';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {  
-  res.writeHead(500, {'Content-type': 'application/json'});
-  res.end(JSON.stringify({message: 'server error'}))
+  res.end('<h1>hello world</h1>');
+  // res.writeHead(500, {'Content-type': 'application/json'});
+  // res.end(JSON.stringify({message: 'server error'}))
 })
 
 server.listen(PORT, () => {
