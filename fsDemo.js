aede234a614e9d7e0f6cd6d9c8ee40812fb88752
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-11 06:09:13 +0800

    node: writeFile() from fs

--- fsDemo.js
+++ fsDemo.js
@@ -1,12 +1,38 @@
-import fs, {readFileSync} from 'fs';
+// import fs, {readFileSync} from 'fs';
+import fs from 'fs/promises'
 
-//Asynchronous version
-fs.readFile('./test.txt', 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-})
+// //Asynchronous version
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// })
 
-//Synchronous version
-const data = readFileSync('./test.txt', 'utf8')
-console.log(data);
+// //Synchronous version
+// const data = readFileSync('./test.txt', 'utf8')
+// console.log(data);
 
+// //Promise verison
+// fs.readFile('./test.txt', 'utf8')
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile('./test.txt', 'utf8')
+    console.log(data);
+  } catch (error) {
+    console.log(err);
+  }
+}
+
+const writeFile = async () => {
+  try {
+    await fs.writeFile('./test.txt', 'Writing to this file')
+    console.log('this file has been written to...')
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+writeFile();
+readFile();
