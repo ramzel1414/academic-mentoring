a0dfbbf66a071beeca296830108aac33d725396f
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-10-13 11:14:26 +0800

    express: Put request

--- routes/posts.js
+++ routes/posts.js
@@ -38,26 +38,53 @@ router.post('/', (req, res) => {
   res.status(201).json(posts);
 })
 
+//Put request handler / Update Post
+router.put('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id)
+  if(!post) {   // if post is not in the database
+    res.status(404).json({msg: `A post not found with an id of ${id}`})
+  } 
+  
+  post.title = req.body.title;   
+  res.status(200).json(posts);
+})
 
 
 
 ////  CLIENT REQUEST SIMULATION  
 
-//sending request / Post method
-const addPost = async (post) => {
-  const response = await fetch('http://localhost:3000/api/posts', {
-      method: 'POST',
+// //sending request / Post method
+// const addPost = async (post) => {
+//   const response = await fetch('http://localhost:3000/api/posts', {
+//       method: 'POST',
+//       headers: {
+//           'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(post), // Send the post object as JSON
+//   });
+  
+//   console.log(await response.json()); // Parse the response data as JSON
+
+
+// };
+// addPost({ title: 'New Post' });   // invoking the addPost function
+
+//sending request / Put method
+const updatePost = async (updatedPost) => {
+  const response = await fetch('http://localhost:3000/api/posts/1', {
+      method: 'PUT',
       headers: {
           'Content-Type': 'application/json',
       },
-      body: JSON.stringify(post), // Send the post object as JSON
+      body: JSON.stringify(updatedPost), // Send the post object as JSON
   });
-  
-  console.log(await response.json()); // Parse the response data as JSON
 
+  console.log('Post updated');
+  console.log(await response.json())
 
 };
-addPost({ title: 'New Post' });   // invoking the addPost function
 
+updatePost({ title: 'Updated Post' });
 
 export default router;
